63af88484aa00278b8e759ade3af4bc29f91141e
Kishan Singh <92136795+kishansingh16@users.noreply.github.com>
2026-08-21 16:21:57 +0530

    added comment

--- src/Lec_13/Reverse_Vowels_of_a_String.java
+++ src/Lec_13/Reverse_Vowels_of_a_String.java
@@ -1,4 +1,10 @@
 package Lec_13;
 
+//Given a string s, reverse only all the vowels in the string and return it.
+//
+//The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+//
+//
+
 public class Reverse_Vowels_of_a_String {
 }
